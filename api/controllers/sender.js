14ac12331c1978f8101cc5db6ecb057def66c1b1
Gang Zhang <gzhangx@hotmail.com>
2020-05-24 22:33:29 -0400

    'done'

--- api/controllers/sender.js
+++ api/controllers/sender.js
@@ -1,23 +1,40 @@
-const autil = require('../../lib/util');
-const sheet = require('../../lib/getSheet');
+const get = require('lodash/get');
+const acccn = require('../../lib/acccn');
 
 const ver = require('../../version');
 
-
+function handleErr(res, err) {
+    const txt = get(err,'response.text');
+    console.log(txt);
+    res.send(txt);
+}
 function checkVideo(req, res) {
-    
+    console.log(req.query);
+    return acccn.recordAcccnVideoViewCount(req.query.id).then(ret=>{
+        res.send(ret);
+    }).catch(err=>{
+        handleErr(res,err);
+    });
+}
+
+function checkChannel(req, res) {
+    const id = req.query.id;
+    console.log(`channel id ${id}`);
+    return acccn.recordAcccnVideoViewCountByChannel(id).then(ret=>{
+        res.send(ret);
+    }).catch(err=>{
+        handleErr(res,err);
+    })
 }
 
 function version(req, res) {
     const date = new Date();
     console.log(`version 1.0 ACCCN Sunday view count${date}`);
-    return res.send(ver);    
+    return res.send(version);    
 }
 
 module.exports = {
-    sender,
-    showWeek,
-    sendHebrewsWeeklyEmail,
-    sendSantury,
+    checkVideo,
+    checkChannel,
     version,
 };
--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const restify = require('restify');
 const route = require('./api/route');
 
 const server = restify.createServer();
+server.use(restify.plugins.queryParser());
 route.route(server);
-
 const port = process.env.PORT || 8080;
 server.listen(port, function() {
   console.log('%s listening at %s', server.name, server.url);
